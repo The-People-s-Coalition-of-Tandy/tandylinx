14d054f6f142eb4cb24697a4add15a374dcd1dcc
SunEinynEeenan <emcafeehahn@gmail.com>
2023-08-03 22:35:14 -0400

    added spotify & apple music

--- Artists/KatMazur/tubis/data.js
+++ Artists/KatMazur/tubis/data.js
@@ -5,18 +5,32 @@ var backgroundColor = "";
  
 var data = [  
     {  
-    "name" : "Youtube", 
-    "url" : "https://youtu.be/PawICnqYctE", 
-    "font_color" : "red", 
-    "bg_color" : "rgb(300 600 50)", 
-    "hover_color" : "rgb(100 100 180)"  
+        "name" : "Youtube", 
+        "url" : "https://youtu.be/PawICnqYctE", 
+        "font_color" : "red", 
+        "bg_color" : "rgb(300 600 50)", 
+        "hover_color" : "rgb(100 100 180)"  
     }, 
     {  
-    "name" : "Bandcamp", 
-    "url" : "https://tandy.bandcamp.com/track/tubis", 
-    "font_color" : "blue", 
-    "bg_color" : "teal", 
-    "hover_color" : "rgb(100 100 180)"  
+        "name" : "Bandcamp", 
+        "url" : "https://tandy.bandcamp.com/track/tubis", 
+        "font_color" : "blue", 
+        "bg_color" : "teal", 
+        "hover_color" : "rgb(100 100 180)"  
+    },
+    {  
+         "name" : "Spotify", 
+        "url" : "https://open.spotify.com/track/5a2a5Qm7dorOQGNxPOqNWG?si=541c5d12690f4b88", 
+        "font_color" : "whitesmoke", 
+        "bg_color" : "green", 
+        "hover_color" : "rgb(10 100 10)"  
+    },
+    {  
+        "name" : "Apple Music", 
+        "url" : "https://tandy.bandcamp.com/track/tubis", 
+        "font_color" : "blue", 
+        "bg_color" : "red", 
+        "hover_color" : "rgb(100 5 5)"  
     },
     {
         "name" :  "PCoT Website",
