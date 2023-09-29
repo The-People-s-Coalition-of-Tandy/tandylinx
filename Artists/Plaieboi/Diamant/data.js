5ac0ff8d8891aa50bd43191f4d069d48bcda655f
SunEinynEeenan <emcafeehahn@gmail.com>
2023-09-29 01:14:29 -0400

    updated with links!

--- Artists/Plaieboi/Diamant/data.js
+++ Artists/Plaieboi/Diamant/data.js
@@ -13,14 +13,14 @@ var data = [
     }, 
     {  
          "name" : "Spotify", 
-        "url" : "google.com", 
+        "url" : "https://open.spotify.com/track/7H0H35jRCxF9U84m9788Rv?si=2cdeb1b8495f4d81", 
         "font_color" : "snow", 
         "bg_color" : "green", 
         "hover_color" : "rgb(10 100 10)"  
     },
     {  
         "name" : "Apple Music", 
-        "url" : "google.com", 
+        "url" : "https://music.apple.com/us/album/diamant-single/1703779771", 
         "font_color" : "whitesmoke", 
         "bg_color" : "darkred", 
         "hover_color" : "rgb(100 5 5)"  
