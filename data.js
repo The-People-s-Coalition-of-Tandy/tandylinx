f97ab64c3ea494666c5ea3bf5691c3b8f211c5b0
julipode <humphreythegray@gmail.com>
2024-09-06 16:41:01 -0400

    add blue dish vid

--- data.js
+++ data.js
@@ -1,30 +1,26 @@
-var data = [
-    {
-        "name" :  'elie mh - No Sun On Main Street',
-        "url" : "https://tandy.bandcamp.com/album/no-sun-on-main-street"
-    },
-    {
-        "name" :  'Chris Weisman – Time Machine',
-        "url" : "https://youtu.be/C-Zrnf_oyuY?si=Ch8ObRFp-ejcVWpT"
-    },
-    {
-        "name" :  '2024 MIDI Competition Webpage',
-        "url" : "https://midicompetition.pcotandy.org/"
-    },
-    {
-        "name" :  '2024 MIDI Competition Album',
-        "url" : "https://tandy.bandcamp.com/album/33rd-general-midi-competition-the-pcot-2024-metaball-lyke-baron-prime-competition-zrainbow-geometrie-740"
-    }, 
-    {
-        "name" :  'tupping time plus by Calvin Grad',
-        "url" : "https://tandy.bandcamp.com/album/tupping-time-plus"
-    },
-    {
-        "name" :  "PCoT Website",
-        "url" : "https://pcotandy.org"
-    },
-    {
-        "name" :  "Get Coalition News",
-        "url" : "https://www.pcotandy.org/signup.html"
-    }//make sure theres no comma at the end of objects or the array
-];
+var data = [{
+    "name": "BLUE DISH Music Video",
+    "url": "https://www.youtube.com/watch?v=t490mC5W7XE"
+},
+{
+    "name": "Chris Weisman – 'How I Wrote Certain Of My Hooks' Score Book",
+    "url": "https://tandy.bandcamp.com/merch/how-i-wrote-certain-of-my-hooks-spiral-bound-cardstock-score-book-by-chris-weisman"
+}, {
+    "name": "Katarina Mazur – Ай ну люлі",
+    "url": "https://linx.pcotandy.org/kmazur"
+}, {
+    "name": "elie mh – No Sun On Main Street",
+    "url": "https://tandy.bandcamp.com/album/no-sun-on-main-street"
+}, {
+    "name": "Chris Weisman – Time Machine",
+    "url": "https://www.youtube.com/watch?si=Ch8ObRFp-ejcVWpT&v=C-Zrnf_oyuY&feature=youtu.be"
+}, {
+    "name": "2024 MIDI Competition Webpage",
+    "url": "http://midicompetition.pcotandy.org/"
+}, {
+    "name": "PCoT Website",
+    "url": "https://pcotandy.org"
+}, {
+    "name": "Get Coalition News",
+    "url": "https://www.pcotandy.org/signup.html"
+}];
